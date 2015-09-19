c883cfd37ce11c12cf826639985247b65f908be0
tpythoner <tpythoner@gmail.com>
2015-09-19 20:09:28 +0800

    add go 1.5

--- node/docker_run.sh
+++ node/docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -i -t -d -p 3000:3000 -p 2002:22 -v /Users/tony/Code/:/home ubuntu:nodejs
+docker run -i -t -d -p 3000:3000 -p 2002:22 -v /Users/tony/Code/:/home ubuntu:node
